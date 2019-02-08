551a590ff567e868a14bb92ac7dcec47941659ac
Daniel Liu <39071861+MrPicklePinosaur@users.noreply.github.com>
2019-02-07 23:20:08 -0500

    assets-commit-part2

---
+++ assets/maps/Tiled/world1_doors_front.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="world1_doors" tilewidth="16" tileheight="16" tilecount="144" columns="12">
+ <image source="../Spritesheets/world1_doors.png" width="192" height="192"/>
+</tileset>
---
+++ assets/maps/Tiled/world1_doors_side.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="world1_doors_side" tilewidth="16" tileheight="16" tilecount="90" columns="9">
+ <image source="../Spritesheets/world1_doors_side.png" width="144" height="160"/>
+</tileset>
